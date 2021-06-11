9a39330ca36d8e5eed56fbadbde20a912c5e8681
Kevin Gross <kcgross4@gmail.com>
2021-06-11 12:46:58 -0600

    Add dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var dinosaurs = {
+  name = "T-Rex";
+  color = "Brown";
+  weightInLbs = 7000;
+  dateAlive = 2001;
+  longArms = false;
+  carnivore = true;
+  prey = ["birds", "stegosaurus", "raptors"]
+  predators = ["velociraptor", "shark", "mammoth"]
+}
+}
