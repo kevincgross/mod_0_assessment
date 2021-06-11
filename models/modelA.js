d5f386879214a91ecb214630d20552cd56a1ad52
Kevin Gross <kcgross4@gmail.com>
2021-06-11 12:49:44 -0600

    Minor change in variable name

--- models/modelA.js
+++ models/modelA.js
@@ -1,11 +1,10 @@
-var dinosaurs = {
-  name = "T-Rex";
+var tyrannosaurusRex = {
+  location = "Africa";
   color = "Brown";
   weightInLbs = 7000;
   dateAlive = 2001;
   longArms = false;
   carnivore = true;
-  prey = ["birds", "stegosaurus", "raptors"]
-  predators = ["velociraptor", "shark", "mammoth"]
-}
+  prey = ["birds", "stegosaurus", "raptors"];
+  predators = ["velociraptor", "shark", "mammoth"];
 }
