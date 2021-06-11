cef1cf5af8f1ad67e4da52a86b46b9cb9eb06661
Kevin Gross <kcgross4@gmail.com>
2021-06-11 13:00:26 -0600

    Add devHabits object

---
+++ models/modelB.js
@@ -0,0 +1,11 @@
+var devTraits = {
+  name: "Kevin Gross",
+  program: "Front-End",
+  timesStudied: 10,
+  questionsAsked: 15,
+  isKeyboardProficient: true,
+  isFallingBehind: false,
+  sessionsReviewed: ["1", "2", "3"],
+  sessionsNotReviewed: ["4", "5", "6"],
+};
+console.log(devTraits)
