ebd305ed6c99407f30eb268c28b240305c964493
Kevin Gross <kcgross4@gmail.com>
2021-06-11 12:59:42 -0600

    Change syntax of object

--- models/modelA.js
+++ models/modelA.js
@@ -1,10 +1,11 @@
 var tyrannosaurusRex = {
-  location = "Africa";
-  color = "Brown";
-  weightInLbs = 7000;
-  dateAlive = 2001;
-  longArms = false;
-  carnivore = true;
-  prey = ["birds", "stegosaurus", "raptors"];
-  predators = ["velociraptor", "shark", "mammoth"];
+  location: "Africa",
+  color: "Brown",
+  weightInLbs: 7000,
+  dateAlive: 2001,
+  longArms: false,
+  carnivore: true,
+  prey: ["birds", "stegosaurus", "raptors"],
+  predators: ["velociraptor", "shark", "mammoth"],
 }
+console.log(tyrannosaurusRex);
